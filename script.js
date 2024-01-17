6fc2c1aaf015baff17ff39f0e1ed5fe55402c0c8
Anthony Cabulang <zyxanthony15@gmail.com>
2024-01-18 01:03:09 +0800

    update 1
    Added playerSelection and computerSelection, as well as the game mechanics, it's working!!

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-let playerSelection = ''
+let playerSelection = 'rock'
 let computerSelection = getComputerChoice()
-
-
 
 
 function getComputerChoice(choice) {
 let choices = ['rock', 'paper', 'scissors']
 choice = choices[Math.floor(Math.random() * choices.length)];
-return choice
-}
+return document.write("Computer's choice is "), choice;
+};
+
+
+function playRound(playerSelection, computerSelection) {
+  if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')) {
+      document.write(`<br> You win, ${playerSelection} defeats ${computerSelection}`);
+    }
+
+  else if(playerSelection === computerSelection) {
+    document.write("<br> There's a tie!");
+  }
+
+  else {
+    document.write("<br> You lose");
+  }
+};
+
+document.write(computerSelection);
+playRound(playerSelection, computerSelection);
